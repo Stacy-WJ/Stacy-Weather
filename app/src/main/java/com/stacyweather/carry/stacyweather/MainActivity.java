d5a810beea69150fbbcf35dc1469f2376c2e76cc
Stacy-WJ <Stacy-WJ@gmail.com>
2017-03-29 22:08:48 +0800

    First commit.

---
+++ app/src/main/java/com/stacyweather/carry/stacyweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.stacyweather.carry.stacyweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
